a915d90f1f913bf28c05d59fe11a460704ec8ae6
Christopher Pavlic <christopher.pavlic@gmail.com>
2015-10-29 14:59:29 +0100

    pavlic camera

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 pos = gameObject.transform.position;
+        pos.x = player.transform.position.x;
+        pos.y = player.transform.position.y;
+        pos.z = transform.position.z;
+        gameObject.transform.position = pos;
+    }
+}

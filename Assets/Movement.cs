f4c090898e230cfd3308232a55c76ff4a00bc5f2
Christopher Pavlic <christopher.pavlic@gmail.com>
2015-10-29 14:14:18 +0100

    Add rotation and acceleration

---
+++ Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float speed = 20f;
+    public float turnSpeed = 5f;
+    private bool up = false;
+    private bool down = false;
+    private bool right;
+    private bool left;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        up = Input.GetKey(KeyCode.UpArrow);
+        down = Input.GetKey(KeyCode.DownArrow);
+        left = Input.GetKey(KeyCode.LeftArrow);
+        right = Input.GetKey(KeyCode.RightArrow);
+    }
+
+    void FixedUpdate () {
+
+        // rotate car
+        if (left)
+            transform.Rotate(Vector3.forward, turnSpeed);
+
+        if (right)
+            transform.Rotate(Vector3.forward, -turnSpeed);
+
+        // add force
+        if (up) {
+            rigidBody.AddForce(transform.up * speed);
+        } else if (down) {
+            rigidBody.AddForce(-transform.up * speed);
+        }
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate () {
+        transform.position += new Vector3(0, 0.1f, 0);
+    }
+}
